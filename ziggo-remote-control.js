0c830c2383114ba4d3a609428fd71fe78b441c94
Bart Kock <bart.kock@sienn.com>
2021-08-31 21:53:58 +0200

    media_play_pause

--- ziggo-remote-control.js
+++ ziggo-remote-control.js
@@ -363,9 +363,9 @@ class ZiggoRemoteControl extends LitElement {
 
 <!-- ################################# MEDIA CONTROL ################################# -->
                  <div class="grid-container-media-control" >
-                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("ziggonext","rewind")}><ha-icon icon="mdi:skip-backward"/></button>
-                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("media_player","MediaPlayPause")}><ha-icon icon="mdi:play-pause"/></button>
-                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("ziggonext","fast_forward")}><ha-icon icon="mdi:skip-forward"/></button>
+                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("media_player","rewind")}><ha-icon icon="mdi:skip-backward"/></button>
+                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("media_player","media_play_pause")}><ha-icon icon="mdi:play-pause"/></button>
+                      <button class="btn-flat flat-low ripple"  @click=${() => this._media_player_service("media_player","fast_forward")}><ha-icon icon="mdi:skip-forward"/></button>
                   </div> 
 <!-- ################################# MEDIA CONTROL END ################################# -->
                   </div>
